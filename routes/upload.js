8981ca074e90762ab4ad01e5ca824913f3086a09
kenoung <E0003273@u.nus.edu>
2016-08-15 00:53:57 +0800

    added simple frontend form validation. simple server side image validation. only check extensions tho

--- routes/upload.js
+++ routes/upload.js
@@ -17,43 +17,67 @@ aws.config.update({
 })
 
 var s3 = new aws.S3();
-var storage = multers3({
-  s3: s3,
-  dirname: '/',
-  bucket: 'giveforfree',
-  acl: 'private',
-  key: function (req, file, cb) {
-    crypto.pseudoRandomBytes(16, function (err, raw) {
-      cb(null, raw.toString('hex') + Date.now() + '.' + mime.extension(file.mimetype));
-    });
-  }
-});
 
 var uploading = multer({
-  storage: storage,
-  limits: { fileSize: 100000 }
-});
+
+  // Only accept image files
+  fileFilter: function (req, file, cb) {
+      console.log(file.mimetype);
+
+      // Checks the file extension
+      if (!(file.mimetype.includes("image"))) {
+        cb(new Error("This is not an image file."), false);
+      } else {
+        cb(null, true)
+      }
+  },
+
+  // Files must be smaller than 100kb
+  limits: { fileSize: 100 * 1000 },
+
+  // Save to Amazon S3 bucket
+  storage: multers3 ({
+                      s3: s3,
+                      dirname: '/',
+                      bucket: 'giveforfree',
+                      acl: 'private',
+                      key: function (req, file, cb) {
+                        crypto.pseudoRandomBytes(16, function (err, raw) {
+                          cb(null, raw.toString('hex') + Date.now() + '.' + 
+                            mime.extension(file.mimetype));
+                        });
+                      }
+                    })
+}).single('avatar');
 
 module.exports = function(app) {
   app.get('/upload', function(req, res) {
-    res.render("upload");
+    res.render("upload", { messages: req.flash('error_messages') });
   });
 
-  app.post('/upload', uploading.single('avatar'), function(req, res) {
-    // Create item based on form
-    var newItem = new db.Item({
-      giverID: req.user.appUserId,
-      timeCreated: moment().format(),
-      timeExpired: moment().add(req.body.no_of_days, 'days').format(),
-      randomAssign: req.body.randomAssign,
-      title: req.body.title,
-      description: req.body.description,
-      imageLocation: req.file.key
+  app.post('/upload', function(req, res) {
+    uploading(req, res, function (err) {
+      if (err) {
+        console.log(err.message);
+        req.flash('error_messages', err.message);
+        res.redirect('/upload');
+      } else {
+        // Create item based on form
+        var newItem = new db.Item({
+          giverID: req.user.appUserId,
+          timeCreated: moment().format(),
+          timeExpired: moment().add(req.body.no_of_days, 'days').format(),
+          randomAssign: req.body.randomAssign,
+          title: req.body.title,
+          description: req.body.description,
+          imageLocation: req.file.key
+        });
+
+        // Save item to database
+        newItem.save();
+
+        res.redirect("/");
+      }
     });
-
-    // Save item to database
-    newItem.save();
-
-    res.redirect("/");
   });
 }
