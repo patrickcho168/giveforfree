60118958377925e875de96de722d822174a53bcc
Huang Lie Jun <liejun.hlj@gmail.com>
2016-08-17 06:38:50 +0800

    no message

--- public/scripts/profile/profile.js
+++ public/scripts/profile/profile.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
 
                         console.log(myFriends[i]);
 
-                        // html += "<li class=\"list-group-item\"><a href=\"/profile/" /*+ myFriends[i].attributes.userID*/ + myFriends[i].attributes.name + "</a></li><br/>";
+                        html += "<li class=\"list-group-item\"><a href=\"/profile/" + myFriends[i].userID + myFriends[i].name + "</a></li><br/>";
                     }
 
                     html += "</ul>";
