a6f31d77315796b4e09df6485b103aff675b2f18
Huang Lie Jun <liejun.hlj@gmail.com>
2016-08-17 07:34:17 +0800

    no message

--- public/scripts/profile/profile.js
+++ public/scripts/profile/profile.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
 
                         console.log(myFriends[i]);
 
-                        html += "<a class=\"list-group-item list-group-item-action\" href=\"/profile/" + myFriends[i].userID + ">" + myFriends[i].name + "<span class=\"tag tag-default tag-pill pull-xs-right\">14</span></a>";
+                        html += "<a class=\"list-group-item list-group-item-action\" href=\"/profile/" + myFriends[i].userID + ">" + myFriends[i].name + "<span class=\"tag tag-default tag-pill pull-xs-right\">" + myFriends[i].name + "</span></a>";
                     }
 
                     html += "</div>";
