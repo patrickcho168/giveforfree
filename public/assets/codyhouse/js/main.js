28743db76c8b514f4b174022f6de98b2f07705a8
Huang Lie Jun <liejun.hlj@gmail.com>
2016-08-18 09:57:47 +0800

    (WIP) Hide menu on profile page

--- public/assets/codyhouse/js/main.js
+++ public/assets/codyhouse/js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 	// browser window scroll (in pixels) after which the "menu" link is shown
-	var offset = $(window).height() * 0.666;
+	var offset = $(window).height();
 
 	var navigationContainer = $('#cd-nav'),
 		mainNavigation = navigationContainer.find('#cd-main-nav ul');
