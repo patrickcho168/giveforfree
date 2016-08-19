d344520715c3a493c0c50ef9493193c0676e5fbc
Huang Lie Jun <liejun.hlj@gmail.com>
2016-08-19 14:30:58 +0800

    (WIP) Item page debugging

--- public/scripts/item/item.js
+++ public/scripts/item/item.js
@@ -65,25 +65,26 @@ $(document).on("click", ".unsnag", function() {
         });
 });
 
+// Carousel Logic
 jQuery(document).ready(function($) {
 
-      $('#myCarousel').carousel({
-              interval: 5000
-      });
+    $('#myCarousel').carousel({
+        interval: false;
+    });
 
-      $('#carousel-text').html($('#slide-content-0').html());
+    $('#carousel-text').html($('#slide-content-0').html());
 
-      //Handles the carousel thumbnails
-     $('[id^=carousel-selector-]').click( function(){
-          var id = this.id.substr(this.id.lastIndexOf("-") + 1);
-          var id = parseInt(id);
-          $('#myCarousel').carousel(id);
-      });
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function() {
+        var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+        var id = parseInt(id);
+        $('#myCarousel').carousel(id);
+    });
 
 
-      // When the carousel slides, auto update the text
-      $('#myCarousel').on('slid.bs.carousel', function (e) {
-               var id = $('.item.active').data('slide-number');
-              $('#carousel-text').html($('#slide-content-'+id).html());
-      });
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid.bs.carousel', function(e) {
+        var id = $('.item.active').data('slide-number');
+        $('#carousel-text').html($('#slide-content-' + id).html());
+    });
 });
