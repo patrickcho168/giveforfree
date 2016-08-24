28868f4304c77b32db1cc4aa3e0b541b1bb1f867
Huang Lie Jun <liejun.hlj@gmail.com>
2016-08-25 07:04:29 +0800

    (WIP) Create Item Page with improved form layout and additional fields

--- public/scripts/upload/upload.js
+++ public/scripts/upload/upload.js
@@ -59,4 +59,80 @@ $(function() {
             reader.readAsDataURL(file);
         }
     });
+
+    $('#date').bootstrapMaterialDatePicker({
+        weekStart: 0,
+        time: false
+    });
+
+    $('textarea').autosize();
+
 });
+
+// $("myDropdown").onblur(function() {
+//     document.getElementById('myDropdown').style.display = 'none';
+// });
+
+function toggle() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("category");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+function changeValue(value) {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("category");
+    input.value = value;
+}
+
+function checkValue() {
+    var input, filter, ul, li, a, i;
+    var categories = [
+        "clothes",
+        "accessories",
+        "furniture & home",
+        "parenting",
+        "health",
+        "beauty",
+        "kitchen appliances",
+        "gardening",
+        "property",
+        "design & craft",
+        "electronics",
+        "sports",
+        "photography",
+        "antiques",
+        "toys",
+        "games",
+        "music",
+        "tickets & vouchers",
+        "auto accessories",
+        "books",
+        "stationery",
+        "textbooks",
+        "notes",
+        "pets",
+        "other"
+    ];
+
+    input = document.getElementById("category");
+
+    var found = $.inArray(input.value, categories);
+
+    if (found == -1) {
+        input.value = "other";
+    }
+}
