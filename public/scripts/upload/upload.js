84d311997615778109b9f32b68656f37baedf7ec
Yang Zhuohan <billstark1996@gmail.com>
2016-10-25 23:30:33 +0800

    no message

--- public/scripts/upload/upload.js
+++ public/scripts/upload/upload.js
@@ -29,24 +29,24 @@ $(document).ready(function() {
 
     $('.donation-input').keyup(function(evt) {
         evt.preventDefault();
-
-        var donationAmount = $('.donation-input').val();
-        var total = parseFloat(donationAmount);
-        var theirs, ours, fee;
-        if (isNaN(total)) {
-            theirs = 0;
-            fee = 0;
-            ours = 0;
-        } else {
-            theirs = culculateTheirs(total);
-            fee = culculateFee(total);
-            ours = culculateOurs(total);
-        }
-        console.log(theirs);
-        console.log(theirs.toFixed(2));
-        $('.actual-amount').text(theirs.toFixed(2));
-        $('.fee').text(fee.toFixed(2));
-        $('.ours').text(ours.toFixed(2));
+        //
+        // var donationAmount = $('.donation-input').val();
+        // var total = parseFloat(donationAmount);
+        // var theirs, ours, fee;
+        // if (isNaN(total)) {
+        //     theirs = 0;
+        //     fee = 0;
+        //     ours = 0;
+        // } else {
+        //     theirs = culculateTheirs(total);
+        //     fee = culculateFee(total);
+        //     ours = culculateOurs(total);
+        // }
+        // console.log(theirs);
+        // console.log(theirs.toFixed(2));
+        // $('.actual-amount').text(theirs.toFixed(2));
+        // $('.fee').text(fee.toFixed(2));
+        // $('.ours').text(ours.toFixed(2));
     });
 });
 
