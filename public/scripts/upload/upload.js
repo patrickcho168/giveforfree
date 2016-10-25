10e5acadd9d818a1ba22402ae0fa7e7ad305b722
Yang Zhuohan <billstark1996@gmail.com>
2016-10-25 23:37:17 +0800

    no message

--- public/scripts/upload/upload.js
+++ public/scripts/upload/upload.js
@@ -13,15 +13,15 @@ $(function() {
 
     $('textarea').autosize();
 
-    var oldScroll = window.onscroll;
-    $(document).on('focus', 'input', function(e) {
-        window.onscroll = function() {
-            window.scroll(0, 0);
-        };
-        setTimeout(function() {
-            window.onscroll = oldScroll;
-        }, 100);
-    });
+    // var oldScroll = window.onscroll;
+    // $(document).on('focus', 'input', function(e) {
+    //     window.onscroll = function() {
+    //         window.scroll(0, 0);
+    //     };
+    //     setTimeout(function() {
+    //         window.onscroll = oldScroll;
+    //     }, 100);
+    // });
 
 });
 
